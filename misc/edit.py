4e6b239c59fcb27be843cbce8822e47318890120
nmaltsev <nsmalcev@gmail.com>
2026-05-06 15:41:39 +0200

    define the text editor prototype

--- misc/edit.py
+++ misc/edit.py
@@ -1,48 +1,67 @@
-import sys
-import os
+from edit_utils import get_key, clear, move_cursor
 
-# ---------- KEY INPUT ----------
-def get_key():
-    try:
-        import msvcrt
-        key = msvcrt.getch()
-
-        if key == b'\xe0':  # special key
-            key = msvcrt.getch()
-            mapping = {
-                b'H': 'UP',
-                b'P': 'DOWN',
-                b'K': 'LEFT',
-                b'M': 'RIGHT'
-            }
-            return mapping.get(key, '')
-
-        return key.decode(errors="ignore")
-
-    except ImportError:
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-            if ch == "\x1b":
-                seq = sys.stdin.read(2)
-                mapping = {
-                    "[A": 'UP',
-                    "[B": 'DOWN',
-                    "[C": 'RIGHT',
-                    "[D": 'LEFT'
-                }
-                return mapping.get(seq, '')
-            return ch
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old)
+def fill(text, max_width):
+    if len(text) >= max_width:
+        return text[0:max_width]
+    else:
+        return text + '-' * (max_width - len(text))
 
 
-# ---------- TERMINAL ----------
-def clear():
-    sys.stdout.write("\x1b[2J\x1b[H")
-    sys.stdout.flush()
+view_box1 = (5,5,40, 10) # x,y, w,h (immutable!)
 
-# TODO read the necessery key presses in the loop
+
+
+def fill_view_box(view_box, content):
+    relative_line_n = 0
+    while relative_line_n < view_box[3]:
+        move_cursor(view_box[0], view_box[1] + relative_line_n)
+        relative_line_n += 1
+        # TODO get content
+        print(fill(f'{relative_line_n=}', view_box[2]), end='')
+    print()
+
+def main():
+    prev = None
+    EDIT_MODE = False
+    cursor_offset = [0,0] # dx, dy
+    buffer = [] # TODO array of  lines 
+    while True:
+        key = get_key()
+         
+        if EDIT_MODE is False:
+            print(f"{key=}")
+        if key == 'CTRL_C' and prev == 'CTRL_C':
+            break
+
+        if key == 'CTRL_W':
+            clear()
+        if key == 'CTRL_P':
+            EDIT_MODE = True
+            fill_view_box(view_box1, buffer)
+            cursor_offset = [0,-1]
+            move_cursor(view_box1[0] + cursor_offset[0], view_box1[1] + cursor_offset[1])
+            print('_', end='')
+            print()
+        
+        if EDIT_MODE is True:
+            if len(key) == 1:
+                move_cursor(view_box1[0] + cursor_offset[0], view_box1[1] + cursor_offset[1])
+                print(key, end='')
+                # TODO print cursor
+                if cursor_offset[0] < view_box1[2]-1:
+                    cursor_offset[0] += 1
+                else: 
+                    cursor_offset[0] = 0
+                    cursor_offset[1] += 1
+                
+                if cursor_offset[1] > view_box1[3]-1:
+                    # TODO call fill_view_box(view_box1)
+                    cursor_offset[1] = view_box1[3] -1
+                print()
+            # TODO handle arrow keys, enter, backspcae and delte
+
+
+        prev = key
+
+if __name__ == '__main__':
+    main()
---
+++ misc/edit_utils.py
@@ -0,0 +1,101 @@
+
+import sys
+import os
+
+def get_key():
+    try:
+        # Windows
+        import msvcrt
+        key = msvcrt.getch()
+
+        # Special keys (arrows, function keys, etc.)
+        if key in (b'\x00', b'\xe0'):
+            key = msvcrt.getch()
+            mapping = {
+                b'H': 'UP',
+                b'P': 'DOWN',
+                b'K': 'LEFT',
+                b'M': 'RIGHT',
+            }
+            return mapping.get(key, '')
+
+        # Control keys
+        if key == b'\x03':
+            return 'CTRL_C'
+        if key == b'\x04':
+            return 'CTRL_D'
+        if key == b'\x08':
+            return 'BACKSPACE'
+        if key == b'\r':
+            return 'ENTER'
+        if key == b'\t':
+            return 'TAB'
+        if key == b'\x1b':
+            return 'ESC'
+
+        # Ctrl + letter (ASCII 1–26)
+        if 1 <= ord(key) <= 26:
+            return f'CTRL_{chr(ord(key) + 64)}'
+
+        return key.decode(errors="ignore")
+
+    except ImportError:
+        # Unix/Linux/macOS
+        import sys
+        import tty
+        import termios
+
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+
+            # Escape sequences (arrows, etc.)
+            if ch == "\x1b":
+                seq = sys.stdin.read(2)
+                print(f"{seq=}")
+                mapping = {
+                    "[A": 'UP',
+                    "[B": 'DOWN',
+                    "[C": 'RIGHT',
+                    "[D": 'LEFT',
+                    '[6': 'PAGE_DOWN',
+                    '[5': 'PAGE_UP',
+                    '[1': 'HOME',
+                    '[4': 'END',
+                    '[2': 'INSERT',
+                    '[3': 'DELETE',
+                }
+                if seq in mapping:
+                    return mapping[seq]
+                return 'ESC'
+
+            # Control keys
+            if ch == '\x03':
+                return 'CTRL_C'
+            if ch == '\x04':
+                return 'CTRL_D'
+            if ch == '\x7f':
+                return 'BACKSPACE'
+            if ch in ('\r', '\n'):
+                return 'ENTER'
+            if ch == '\t':
+                return 'TAB'
+
+            # Ctrl + letter
+            if 1 <= ord(ch) <= 26:
+                return f'CTRL_{chr(ord(ch) + 64)}'
+
+            return ch
+
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
+# ---------- TERMINAL ----------
+def clear():
+    sys.stdout.write("\x1b[2J\x1b[H")
+    sys.stdout.flush()
+
+def move_cursor(x, y):
+    sys.stdout.write(f"\x1b[{y+1};{x+1}H")
